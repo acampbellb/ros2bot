c31198e43f6c91430b38c621a3c324e5b625df79
Adam Campbell <acampbellb@hotmail.com>
2023-01-21 19:09:16 -0500

    changes

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,104 +1,19 @@
 #!/usr/bin/env bash
 
-CONTAINER_IMAGE="nvcr.io/nvidian/nvidia-l4t-base:r32.4"
+#
+# This script runs the docker ROS container image.
+#
 
-USER_VOLUME=""
-USER_COMMAND=""
+CONTAINER_IMAGE=""
+WORKSPACE_NAME="ros_ws"
+WORKSPACE_HOST="/home/ros2bot/src"
+WORKSPACE_CONTAINER="/home/ros/ros_ws/src/:rw"
+INITIAL_COMMAND="/bin/bash"
 
-show_help() {
-    echo " "
-    echo "usage: Starts the Docker container and runs a user-specified command"
-    echo " "
-    echo "   ./scripts/docker_run.sh --container DOCKER_IMAGE"
-    echo "                           --volume HOST_DIR:MOUNT_DIR"
-    echo "                           --run RUN_COMMAND"
-    echo " "
-    echo "args:"
-    echo " "
-    echo "   --help                       Show this help text and quit"
-    echo " "
-    echo "   -c, --container DOCKER_IMAGE Specifies the name of the Docker container"
-    echo "                                image to use (default: 'nvidia-l4t-base')"
-    echo " "
-    echo "   -v, --volume HOST_DIR:MOUNT_DIR Mount a path from the host system into"
-    echo "                                   the container.  Should be specified as:"
-    echo " "
-    echo "                                      -v /my/host/path:/my/container/path"
-    echo " "
-    echo "   -r, --run RUN_COMMAND  Command to run once the container is started."
-    echo "                          Note that this argument must be invoked last,"
-    echo "                          as all further arguments will form the command."
-    echo "                          If no run command is specified, an interactive"
-    echo "                          terminal into the container will be provided."
-    echo " "
-}
-
-die() {
-    printf '%s\n' "$1"
-    show_help
-    exit 1
-}
-
-# parse arguments
-while :; do
-    case $1 in
-        -h|-\?|--help)
-            show_help    # Display a usage synopsis.
-            exit
-            ;;
-        -c|--container)       # Takes an option argument; ensure it has been specified.
-            if [ "$2" ]; then
-                CONTAINER_IMAGE=$2
-                shift
-            else
-                die 'ERROR: "--container" requires a non-empty option argument.'
-            fi
-            ;;
-        --container=?*)
-            CONTAINER_IMAGE=${1#*=} # Delete everything up to "=" and assign the remainder.
-            ;;
-        --container=)         # Handle the case of an empty --image=
-            die 'ERROR: "--container" requires a non-empty option argument.'
-            ;;
-        -v|--volume)
-            if [ "$2" ]; then
-                USER_VOLUME=" -v $2 "
-                shift
-            else
-                die 'ERROR: "--volume" requires a non-empty option argument.'
-            fi
-            ;;
-        --volume=?*)
-            USER_VOLUME=" -v ${1#*=} " # Delete everything up to "=" and assign the remainder.
-            ;;
-        --volume=)         # Handle the case of an empty --image=
-            die 'ERROR: "--volume" requires a non-empty option argument.'
-            ;;
-        -r|--run)
-            if [ "$2" ]; then
-                shift
-                USER_COMMAND=" $@ "
-            else
-                die 'ERROR: "--run" requires a non-empty option argument.'
-            fi
-            ;;
-        --)              # End of all options.
-            shift
-            break
-            ;;
-        -?*)
-            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
-            ;;
-        *)               # Default case: No more options, so break out of the loop.
-            break
-    esac
-
-    shift
-done
-
-echo "CONTAINER_IMAGE: $CONTAINER_IMAGE"
-echo "USER_VOLUME:     $USER_VOLUME"
-echo "USER_COMMAND:    '$USER_COMMAND'"
+echo "CONTAINER_IMAGE:     $CONTAINER_IMAGE"
+echo "WORKSPACE_HOST:      $WORKSPACE_HOST"
+echo "WORKSPACE_CONTAINER: $WORKSPACE_CONTAINER"
+echo "INITIAL_COMMAND:     '$INITIAL_COMMAND'"
 
 # give docker root user X11 permissions
 sudo xhost +si:localuser:root
@@ -108,8 +23,23 @@ XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 chmod 777 $XAUTH
 
-# run the container
-sudo docker run --runtime nvidia -it --rm --network host -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix \
-    -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
-    $USER_VOLUME $CONTAINER_IMAGE $USER_COMMAND
+# create a shared volume to store the ros_ws
+docker volume create --driver local \
+    --opt type="none" \
+    --opt device="${PWD}/src/" \
+    --opt o="bind" \
+    "${WORKSPACE_NAME}_src_vol"
+
+# run the container image
+sudo docker run \
+    --runtime nvidia \
+    -it \                                                           # interactive mode, pseudo tty
+    --rm \                                                          # remove container when exits
+    --network host \                                                # network container connects to
+    -e DISPLAY=$DISPLAY \                                           # set display environment variable
+    -v /tmp/.X11-unix/:/tmp/.X11-unix \                             # x11 for SSH
+    -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \                         # x11 authorization
+    -volume="${WORKSPACE_NAME}_src_vol:/home/ros/ros_ws/src/:rw" \  # ros workspace bind mount volume
+    #-v $WORKSPACE_HOST:$WORKSPACE_CONTAINER \ 
+    $CONTAINER_IMAGE $INITIAL_COMMAND
+
