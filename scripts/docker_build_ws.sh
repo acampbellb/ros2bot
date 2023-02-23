aed30a75386af401a6f541b1990851a295f00377
Adam Campbell <acampbellb@hotmail.com>
2023-02-23 12:58:39 -0500

    changes

--- scripts/docker_build_ws.sh
+++ scripts/docker_build_ws.sh
@@ -2,9 +2,9 @@
 
 build_ros()
 {
-	local base_image="acampbellb/ros2bot:foxy-desktop-l4t-ros2bot-base"
-    local dockerfile="Dockerfile-dev"
-    local container_tag="acampbellb/ros2bot:foxy-desktop-l4t-ros2bot-dev"
+	local base_image="acampbellb/ros2bot:foxy-desktop-l4t-ros2bot-dev"
+    local dockerfile="Dockerfile-ws"
+    local container_tag="acampbellb/ros2bot:foxy-desktop-l4t-ros2bot-ws"
 
 	echo ""
 	echo "Building container $container_tag"
@@ -21,5 +21,3 @@ build_ros()
 }
 
 build_ros $BASE_IMAGE
-
-
